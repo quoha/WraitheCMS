93992b4a5db4ec5d94d9053cb4e52b0d578c7e09
Michael Henderson <mdhender@lolian.local>
2013-01-17 09:25:12 -0700

    Initial Commit

---
+++ WraitheCMS/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  WraitheCMS
+//
+//  Created by Michael Henderson on 1/17/13.
+//  Copyright (c) 2013 Michael D Henderson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
